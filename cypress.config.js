1f563ae44847140a141b90c89cb2cbcda0c23e26
Camila Pinheiro <161722302+cam-pinheiro@users.noreply.github.com>
2024-06-13 19:34:35 +0200

    update workflow

--- cypress.config.js
+++ cypress.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents,
     specPattern: "cypress/e2e/features/*.feature",
-    projectId: "r5m4ny",
+    projectId: "ssu1js", //CYPRESS CLOUD PROJECT ID
     baseUrl: "https://www.bootcampqa.com/",
     chromeWebSecurity: true
   },
