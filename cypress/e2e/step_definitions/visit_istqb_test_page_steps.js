9dfee71dea18b1325f0accf414c60469d3ff6782
Camila Pinheiro <161722302+cam-pinheiro@users.noreply.github.com>
2024-06-25 19:48:19 +0200

    Update visit_istqb_test_page_steps.js

--- cypress/e2e/step_definitions/visit_istqb_test_page_steps.js
+++ cypress/e2e/step_definitions/visit_istqb_test_page_steps.js
@@ -11,4 +11,20 @@ Given("the student is on the istqb test page", () => {
 // cuando esta definida en cypress.confg.js la base url
 cy.visit("/")
 
+//borrar todas las cookies
+cy.clearCookies();
+
+//recargar una pagina
+cy.reload();
+
+//coger un elemento de la pagina con get
+//por clase
+cy.get(".nav-link");
+//por tipo de elemento
+cy.get("a");
+// por id
+cy.get('#welcomelink');
+//por atributo o valor
+cy.get('a[href="#welcome"]')
+// por data-cy o data-test_ el mas recomendado para usar!!!
 })
